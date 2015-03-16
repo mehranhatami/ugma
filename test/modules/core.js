b92483f24d97ea706fe082cd6cb75d95754173af
KFlash <kflash123@gmail.com>
2015-03-17 07:04:22 +0800

    improved test case

--- test/modules/core.js
+++ test/modules/core.js
@@ -1,17 +1,26 @@
 describe("core", function() {
     "use strict";
 
-    it("should output <document> as the root node", function() {
-        expect(ugma.toString()).toBe('<document>');
-    });
-
-    it("should output the codename", function() {
-        expect(ugma.codename).toBe('trackira');
+    it("should be a object", function() {
+        expect(typeof ugma).toBe('object');
     });
 
     it("should be nodeType 1 for root node", function() {
         expect(ugma[0].nodeType).toBe(1);
     });
 
-    
-    });    
+    it("should have a overloaded toString", function() {
+        expect(ugma.toString()).toBe('<document>');
+        expect(typeof ugma.toString()).toBe('string');        
+    });
+
+    it("should output the version number", function() {
+        expect(ugma.version).toBe('0.0.1');
+        expect(typeof ugma.version).toBe('string');        
+    });
+
+    it("should output the codename", function() {
+        expect(ugma.codename).toBe('trackira');
+        expect(typeof ugma.codename).toBe('string');        
+    });
+ });    
