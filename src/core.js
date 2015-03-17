2d29e9c0e92913609cf698fad54af69a4f3d9c85
KFlash <kflash123@gmail.com>
2015-03-18 06:31:44 +0800

    simplifying

--- src/core.js
+++ src/core.js
@@ -1,38 +1,51 @@
 import { WINDOW, DOCUMENT } from "./const";
 import { uClass } from "./uclass";
-var Node;
 
-var Element = uClass({
-    constructor: function(node) {
-        if (this instanceof Element) {
-            node["<%= prop() %>"] = this;
-            this[0] = node;
-            this._ = {};
+var Node, Document,
+    Element = uClass({
+        constructor: function(node) {
+            if (this instanceof Element) {
+                node["<%= prop() %>"] = this;
+                this[0] = node;
+                this._ = {};
 
-        } else {
-            return node ? node["<%= prop() %>"] || new Element(node) : new Node();
+            } else {
+                return node ? node["<%= prop() %>"] || new Element(node) : new Node();
+            }
+        },
+        // all of these placeholder strings will be replaced by gulps's
+        version: "<%= pkg.version %>",
+        codename: "<%= pkg.codename %>",
+        toString() {
+            var node = this[0];
+            return node && node.tagName ? "<" + node.tagName.toLowerCase() + ">" : "";
+        },
+        // Create a ugma wrapper object for a native DOM element or a
+        // jQuery element. E.g. (ugma.native($('#foo')[0]))
+        native(node) {
+            var nodeType = node && node.nodeType;
+            // filter non elements like text nodes, comments etc.
+            return (nodeType === 9 ?
+                    Document :
+                    Element
+                )
+                (
+                    nodeType === 1 ||
+                    nodeType === 9 ?
+                    node :
+                    null
+                );
         }
-    },
-    // all of these placeholder strings will be replaced by gulps's
-    version: "<%= pkg.version %>",
-    codename: "<%= pkg.codename %>",
-    toString() {
-        var node = this[0];
-        return node && node.tagName ? "<" + node.tagName.toLowerCase() + ">" : "";
-    }
-});
-
+    });
 
 Node = uClass(Element, {
     constructor: function() {},
     toString: function() {
         return "";
     }
-
 });
 
-
-var Document = uClass(Element, {
+Document = uClass(Element, {
     constructor: function(node) {
         return Element.call(this, node.documentElement);
     },
--- src/uclass.js
+++ src/uclass.js
@@ -1,60 +1,38 @@
-
-  var extend = function (obj, extension, override) {
+var extend = function(obj, extension, override) {
     var prop;
     if (override === false) {
-      for (prop in extension)
-        if (!(prop in obj))
-          obj[prop] = extension[prop];
+        for (prop in extension)
+            if (!(prop in obj))
+                obj[prop] = extension[prop];
     } else {
-      for (prop in extension)
-        obj[prop] = extension[prop];
-      if (extension.toString !== Object.prototype.toString)
-        obj.toString = extension.toString;
+        for (prop in extension)
+            obj[prop] = extension[prop];
     }
-  };
+};
 
-
-  var extendClass = function (Class, extension, override) {
-    if (extension.STATIC) {
-      extend(Class, extension.STATIC, override);
-      delete extension.STATIC;
-    }
-    extend(Class.prototype, extension, override);
-  };
-
-// Export it
-
-export function uClass () {
-  var len = arguments.length,
-     body = arguments[len - 1],
-     SuperClass = len > 1 ? arguments[0] : null,
-     hasImplementClasses = len > 2,
-     Class, SuperClassEmpty;
+export function uClass() {
+    var len = arguments.length,
+        body = arguments[len - 1],
+        SuperClass = len > 1 ? arguments[0] : null,
+        Class, SuperClassEmpty;
 
     if (body.constructor === Object) {
-      Class = function() {};
+        Class = function() {};
     } else {
-      Class = body.constructor;
-      delete body.constructor;
+        Class = body.constructor;
+        delete body.constructor;
     }
 
     if (SuperClass) {
-      SuperClassEmpty = function() {};
-      SuperClassEmpty.prototype = SuperClass.prototype;
-      Class.prototype = new SuperClassEmpty();
-      Class.prototype.constructor = Class;
-      Class.Super = SuperClass;
-      extend(Class, SuperClass, false);
+        SuperClassEmpty = function() {};
+        SuperClassEmpty.prototype = SuperClass.prototype;
+        Class.prototype = new SuperClassEmpty();
+        Class.prototype.constructor = Class;
+        Class.Super = SuperClass;
+        extend(Class, SuperClass, false);
     }
-    
-    
-     if (hasImplementClasses)
-      for (var i = 1; i < len - 1; i++)
-        extend(Class.prototype, arguments[i].prototype, false);    
 
-    extendClass(Class, body);
+    extend(Class.prototype, body);
 
     return Class;
-  }
-
-
+}
