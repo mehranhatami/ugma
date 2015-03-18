db985228b24747d48b915cce424be0ee0a9dbcca
KFlash <kflash123@gmail.com>
2015-03-18 18:36:52 +0800

    fixed minErr() module issues

--- src/minErr.js
+++ src/minErr.js
@@ -1,7 +1,9 @@
 import { ERROR_MSG } from "./const";
 
 export function minErr(module, msg) {
-    var wrapper = () => {
+    // NOTE! The 'es6transpiller' will convert 'this' to '$this0' if we try to
+    // use the arrow method here. And the function will fail BIG TIME !!
+    var wrapper = function() {
         this.message = module ? (msg ? msg : ERROR_MSG[4]) +
             (module.length > 4 ? " -> Module: " + module : " -> Core ") : ERROR_MSG[1];
         // use the name on the framework
