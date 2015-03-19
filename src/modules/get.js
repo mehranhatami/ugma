725043c4f40efdf91f966b782ef2556be312590a
KFlash <kflash123@gmail.com>
2015-03-19 22:50:47 +0800

    refactored dataAttr() method

--- src/modules/get.js
+++ src/modules/get.js
@@ -8,16 +8,26 @@ var multiDash = /([A-Z])/g,
         // convert from camel case to dash-separated value
         key = "data-" + key.replace(multiDash, "-$1").toLowerCase();
 
-        var value = node.getAttribute(key);
-
-        if (value != null) {
-            // try to recognize and parse object notation syntax
-            if (value[0] === "{" && value[value.length - 1] === "}") {
+        var value = node.getAttribute(key),
+            parseJSON = (value)  => {
                 try {
                     value = JSON.parse(value);
                 } catch (err) {}
-            }
+                return value;
+            };
+
+        if (value != null) {
+
+				value = value === "true" ? true :
+					value === "false" ? false :
+					value === "null" ? null :
+					// Only convert to a number if it doesn't change the string
+					+value + "" === value ? +value :
+ 			        // try to recognize and parse object notation syntax
+            		value[0] === "{" && value[value.length - 1] === "}" ? parseJSON(value) :
+					value;
         }
+
         return value;
     };
 
