30e101997d6ee810cbc620210824348c6d5b51ec
KFlash <kflash123@gmail.com>
2015-03-19 08:15:01 +0800

    refactored contains()

--- src/modules/contains.js
+++ src/modules/contains.js
@@ -3,24 +3,24 @@ import { minErr } from "../minErr";
 import { Element } from "../core";
 import { RETURN_FALSE } from "../const";
 
-// Referance: https://dom.spec.whatwg.org/#dom-node-comparedocumentposition 
 implement({
-    // Test whether an element contains another element in the DOM.
+    // The contains(other) method returns true if other is an inclusive descendant of the context object,
+    // and false otherwise (including when other is null).
+    // Referance: https://dom.spec.whatwg.org/#dom-node-comparedocumentposition 
     contains(other) {
-        // Let reference be the context object.
+        // let reference be the context object.
         var reference = this[0];
 
         if (other instanceof Element || (other && other.nodeType === 1)) {
+
             other = (other.nodeType === 1) ? other : other[0];
+
             // If other and reference are the same object, return zero.
             if (other === reference) return 0;
 
-            /* istanbul ignore else */
-            if (reference.contains) {
-                return reference.contains(other) !== null;
-            }
-            // Return true if reference is an ancestor of other.
-            return !!(reference.compareDocumentPosition(other) & 16); // 10 in hexadecimal
+            return reference.contains ?
+                reference.contains(other) !== null :
+                !!(reference.compareDocumentPosition(other) & 16);
         }
         minErr("contains()", "Comparing position against non-Node values is not allowed.");
     }
