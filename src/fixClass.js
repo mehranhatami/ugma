4a8b3a9253e932a24236f81a9eae6a662502a5ed
Mehran Hatami <mehranhatamie@gmail.com>
2015-03-21 22:40:04 +0330

    improve create function and working examples

--- src/fixClass.js
+++ src/fixClass.js
@@ -1,70 +1,81 @@
-// import { forOwn } from "./helpers";
+import { create, extend } from "./create";
 
-// Mehran! This broke Travis
-// "use strict";
-
-var forOwn = function(){};
-
-function newObject(proto, FN, args) {
-  var obj = Object.create(proto);
+function newObject(proto, Base, FN, args) {
+  var obj = create(proto);
   if (typeof FN === "function") {
     FN.apply(obj, args);
   }
   return obj;
 }
 
-function extend(obj, extension, preserve) {
-  // failsave if something goes wrong
-  if (!obj || !extension) return obj || extension || {};
-
-  forOwn(extension, function(prop, func){
-    // if preserve is set to true, obj will not be overwritten by extension if
-    // obj has already a method key
-    obj[prop] = (preserve === false && !(prop in obj)) ? func : func;
-
-    if (preserve && extension.toString !== Object.prototype.toString) {
-      obj.toString = extension.toString;
-    }
-  });
-}
-
 function fixClass(Base, constructorFN, info) {
-  var proto = Object.create(Base && Base.prototype);
-
+  var proto = create(Base && Base.prototype);
   extend(proto, info);
 
-  function FN() {
-    return newObject(proto, constructorFN, arguments);
+  function FN(args) {
+    return newObject(proto, Base, constructorFN, args);
   }
 
-  if (typeof proto.new === "undefined") {
-    proto.new = function () {
-      //No need to use new keyword although you could use it
-      return FN();
-    };
-  }
-
+  FN.new = function () {
+    return FN(arguments);
+  };
   FN.prototype = proto;
   proto.constructor = FN;
   return FN;
 }
 
 function fixClass2(Base, FN, info) {
-  var proto = Object.create(Base && Base.prototype);
-
+  var proto = create(Base && Base.prototype);
   extend(proto, info);
 
-  if (typeof proto.new === "undefined") {
-    proto.new = function () {
-      return newObject(proto, FN, arguments);
-    };
-  }
+  FN.new = function () {
+    return newObject(proto, FN, arguments);
+  };
 
   FN.prototype = proto;
   proto.constructor = FN;
   return FN;
 }
 
-var MyArray = fixClass(Array, function () {
+//It creates a new Function as the Class function itself
+function firstSolution(){
+  var MyArray = fixClass(Array, function () {
 
-});
+  }, {
+    toString: function(){
+      return "[Object MyArray]";
+    }
+  });
+
+  var array = MyArray.new();
+
+  array.push(11,22,33,44);
+
+  console.log('MyArray:'+ array);
+
+  return array;
+}
+
+//It doesn't createsa new Function for the Class, it just works on the existing function
+function secondSolution(){
+  var MyOtherArray = function(){
+
+  };
+  MyOtherArray.prototype.toString = function(){
+    return "[Object MyOtherArray]";
+  };
+
+  fixClass2(Array, MyOtherArray, {
+    getString: function(){
+      return this.toString();
+    }
+  });
+
+  var array = MyOtherArray.new();
+
+  array.push(111,222,333,444);
+
+  console.log('MyOtherArray:' + array);
+
+  return array;
+}
