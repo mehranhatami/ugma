3886e791aa6a1edf1f4ddf028d7dcbe995a5dada
KFlash <kflash123@gmail.com>
2015-03-22 13:30:05 +0800

    re-factored add()
    - now support creation of new DOM elements
    - added test cases

--- src/modules/add.js
+++ src/modules/add.js
@@ -10,57 +10,55 @@ implement({
     // Create a new array of Element from Emmet or HTML string in memory
     addAll: "All"
 
-}, (methodName, all) => function(value, varMap, attributes, styles) {
+}, (methodName, all) => function(value, varMap) {
 
-    var doc = this[0].ownerDocument,
-        sandbox = this._["<%= prop('sandbox') %>"] || (this._["<%= prop('sandbox') %>"] = doc.createElement("div"));
+    if (is(value, "string")) {
 
-    var nodes, el;
+        var doc = this[0].ownerDocument,
+            sandbox = this._["<%= prop('sandbox') %>"] || (this._["<%= prop('sandbox') %>"] = doc.createElement("div"));
 
-    if (value && value in tagCache) {
+        var nodes, el;
 
-        if (!is(value, "string")) {
-            minErr(methodName + "()", ERROR_MSG[7]);
-        }
+        if (value && value in tagCache) {
 
-        nodes = doc.createElement(value);
+            nodes = doc.createElement(value);
 
-        if (all) nodes = [new Element(nodes)];
-    } else {
-        value = trim(value);
-        // handle vanila HTML strings
-        // e.g. <div id="foo" class="bar"></div>
-        if (value[0] === "<" && value[value.length - 1] === ">" && value.length >= 3) {
-            
-            value = varMap ? ugma.format(value, varMap) : value;
-            
-            // styles
-            if (styles && is(styles, "object")) value.css(styles);
-            
-            // attributes
-            if (attributes && is(attributes, "object")) value.set(attributes);
-            
-        } else { // emmet strings
-            value = ugma.emmet(value, varMap);
-        }
-
-        sandbox.innerHTML = value; // parse input HTML string
-
-        nodes = all ? [] : null;
-
-        if (sandbox.childNodes.length === 1 && sandbox.firstChild.nodeType === 1) {
-            nodes = sandbox.removeChild(sandbox.firstChild);
+            if (all) nodes = [new Element(nodes)];
         } else {
+            value = trim(value);
+            // handle vanila HTML strings
+            // e.g. <div id="foo" class="bar"></div>
+            if (value[0] === "<" && value[value.length - 1] === ">" && value.length >= 3) {
 
-            for (; el = sandbox.firstChild;) {
-                sandbox.removeChild(el); // detach element from the sandbox
+                value = varMap ? ugma.format(value, varMap) : value;
 
-                if (el.nodeType === 1) {
-                    nodes.push(new Element(el));
+            } else { // emmet strings
+                value = ugma.emmet(value, varMap);
+            }
+
+            sandbox.innerHTML = value; // parse input HTML string
+
+            nodes = all ? [] : null;
+
+            if (sandbox.childNodes.length === 1 && sandbox.firstChild.nodeType === 1) {
+                nodes = sandbox.removeChild(sandbox.firstChild);
+            } else {
+
+                for (; el = sandbox.firstChild;) {
+                    sandbox.removeChild(el); // detach element from the sandbox
+
+                    if (el.nodeType === 1) {
+                        nodes.push(new Element(el));
+                    }
                 }
             }
         }
+        return all ? nodes : Element(nodes);
     }
 
-    return all ? nodes : Element(nodes);
+    if (value.nodeType !== 1) {
+        minErr("add()", "Not supported");
+    }
+
+    return Element(value);
 });
---
+++ test/modules/add.js
@@ -0,0 +1,93 @@
+describe("add", function() {
+    "use strict";
+    
+     it("should create new DOM element if the first argument is native element", function() {
+        var el = ugma.add(document.createElement("em"));
+
+        jasmine.sandbox.set(el);
+
+        expect(el).toHaveTag("em");
+    });
+    
+    it("should add single ugma element if parameter is not an HTML string", function() {
+        var link = ugma.add(ugma.format("<a id=\"{id}\" title=\"{title}\"></a>", {id: "b", title: "c"}));
+
+        jasmine.sandbox.set(link);
+
+        expect(link).toHaveTag("a");
+        expect(link.get("id")).toBe("b");
+        expect(link.get("title")).toBe("c");
+    });
+
+    it("should parse HTML strings", function() {
+        var el = ugma.add("<a><span></span></a>");
+
+        jasmine.sandbox.set(el);
+
+        expect(el).toHaveTag("a");
+        expect(el.child(0)).toHaveTag("span");
+
+        expect(ugma.addAll("a+b").length).toBe(2);
+    });
+    
+
+    it("should accept empty strings", function() {
+        var el = ugma.add("");
+
+        expect(el).toBeDefined();
+        expect(el[0]).not.toBeDefined();
+    });
+
+    it("should trim inner html strings", function() {
+        var el = ugma.add("   <a><span></span></a>  ");
+
+        expect(el).toHaveTag("a");
+        expect(el.child(0)).toHaveTag("span");
+    });
+
+    it("should support varMap for HTML strings", function() {
+        var el = ugma.add("<a>{0}</a>", ["yo"]);
+
+        expect(el).toHaveTag("a");
+        expect(el.get()).toBe("yo");
+    });
+
+    it("should parse emmet-like expressions", function() {
+        var el = ugma.add("ul>li");
+
+        jasmine.sandbox.set(el);
+
+        expect(el).toHaveTag("ul");
+        expect(el.child(0)).toHaveTag("li");
+    });
+
+    it("should throw error if argument is invalid", function() {
+        expect(function() { ugma.add(2); }).toThrow();
+        // expect(function() { ugma.add(null); }).toThrow();
+        expect(function() { ugma.add({}); }).toThrow();
+    });
+
+    describe("addAll", function() {
+        it("should always return array of elements", function() {
+            var els = ugma.addAll("a");
+
+            expect(Array.isArray(els)).toBeTruthy();
+            expect(els[0]).toHaveTag("a");
+        });
+
+        it("should wrap element to div if HTML string has several root nodes", function() {
+            var el = ugma.addAll("a+b");
+
+            expect(el[0]).toHaveTag("a");
+            expect(el[1]).toHaveTag("b");
+        });
+
+        it("should skip non elements", function() {
+            var links = ugma.addAll("a+`text`+a");
+
+            expect(links.length).toBe(2);
+            expect(links[0]).toHaveTag("a");
+            expect(links[1]).toHaveTag("a");
+        });
+    });
+});
