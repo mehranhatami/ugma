9bf9e2abdfb78b275973948a1c6d0c653f5d4f0b
KFlash <kflash123@gmail.com>
2015-03-23 08:04:26 +0800

    re-factor value

--- src/modules/value.js
+++ src/modules/value.js
@@ -1,17 +1,18 @@
-import { implement, is, every } from "../helpers";
-import { RETURN_THIS } from "../const";
+import { implement, isArray } from "../helpers";
 import { Element } from "../core";
 
 implement({
-    // Replace child nodes of current element
+    // Read or write inner content of the element
     value(val) {
         if (arguments.length === 0) {
             return this.get();
-        } else if ((val instanceof Element) || Array.isArray(val)) {
-            return this.set("").append(val);
-        } else if (is(val, "string")) {
-            return this.set(val);
         }
+
+        if (val._ || isArray(val)) {
+            return this.set("").append(val);
+        }
+
+       return this.set(val);
     }
 }, null, () => function() {
     if (arguments.length) return this;
