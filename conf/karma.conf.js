d3f6ba5f08e00f43877f73a656dfd118251bf605
KFlash <kflash123@gmail.com>
2015-03-24 19:56:08 +0800

    karma.conf.js fix

--- conf/karma.conf.js
+++ conf/karma.conf.js
@@ -6,15 +6,30 @@ module.exports = function(config) {
         singleRun: true,
         frameworks: ["jasmine"],
         browsers: ["PhantomJS"],
-        preprocessors: { "build/ugma.js": "coverage" }, // coverage not set up. Need a account!!
+        preprocessors: {
+            "build/ugma.js": "coverage"
+        }, // coverage not set up. Need a account!!
         coverageReporter: {
             type: "html",
             dir: "coverage/"
         },
         files: [
-           "./test/lib/jasmine-tools.js",
+            "./test/lib/jasmine-tools.js",
             "./build/ugma.js",
             "./test/modules/**/*.js"
-        ]
+        ],
+        // default configuration, not required 
+        traceurPreprocessor: {
+            // options passed to the traceur-compiler 
+            // see traceur --longhelp for list of options 
+            options: {
+                sourceMaps: false,
+                modules: 'amd'
+            },
+            // custom filename transformation function 
+            transformPath: function(path) {
+                return path.replace(/\.es6$/, '.js');
+            }
+        }
     });
 };
