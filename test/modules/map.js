b061e7dcbe98d1fc07fc8f84f6f289978c2745d6
KFlash <kflash123@gmail.com>
2015-03-25 22:37:53 +0800

    couple of test cases

---
+++ test/modules/map.js
@@ -0,0 +1,37 @@
+describe("map", function() {
+    var el;
+
+    beforeEach(function() {
+        el = ugma.add("a");
+    });
+
+    it("applies function and returns array", function() {
+        var spy = jasmine.createSpy("spy"),
+            result = el.map(spy.and.returnValue("bar"));
+
+        expect(spy).toHaveBeenCalledWith(el);
+        expect(result).toEqual(["bar"]);
+    });
+
+    it("supports optional argument context", function() {
+        var context = {};
+
+        el.map(function() {
+            expect(this).toBe(context);
+        }, context);
+    });
+
+    it("returns empty array for dummy nodes", function() {
+        var spy = jasmine.createSpy("spy").and.returnValue("ok");
+
+        expect(ugma.mock().map(spy)).toEqual([]);
+        expect(ugma.query("foo").map(spy)).toEqual([]);
+
+        expect(spy).not.toHaveBeenCalled();
+    });
+
+    it("throws error if the first argument is not a function", function() {
+        expect(function() { el.map(1) }).toThrow();
+        expect(function() { el.map({}) }).toThrow();
+    });
+});
