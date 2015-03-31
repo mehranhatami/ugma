114b81670362f77a190ed401a2b1f65798b758cc
KFlash <kflash123@gmail.com>
2015-03-31 21:07:15 +0800

    fixed a boolean attribute bug / issue

--- src/modules/clear.js
+++ src/modules/clear.js
@@ -11,8 +11,10 @@ implement({
 
         let node = this[0],
             lower = name.toLowerCase();
-            
-        if (BOOLEANS[lower]) {
+        // Boolean attributes get special treatment, except
+        // 'checked'. Because in Firefox the selcted value has 1, but
+        // in Crome the selected value is 2.
+        if (lower !== "checked" && BOOLEANS[lower]) {
             // Boolean attributes need to be set to 'false' before removed
             node[name] = false;
             node.removeAttribute(lower);
