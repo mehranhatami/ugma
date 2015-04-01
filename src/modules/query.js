34792661969f35c0816477255374aeadaef6b988
KFlash <kflash123@gmail.com>
2015-04-01 11:21:35 +0800

    matching query test cases

--- src/modules/query.js
+++ src/modules/query.js
@@ -3,45 +3,9 @@ import { nodeTree, dummyTree } from "../core";
 import { minErr } from "../minErr";
 import { is, map, implement, invoke } from "../helpers";
 
-var unionSplit = /([^\s,](?:"(?:\\.|[^"])+"|'(?:\\.|[^'])+'|[^,])*)/g,
-    rquick = /^(?:(\w+)|\.([\w\-]+))$/,
-    rescape = /'|\\/g,
-    useRoot = function(context, query, method) {
-
-        // create a temporary id for rooted qSA queries
-        var oldContext = context,
-            old = context.getAttribute("id"),
-            nid = old || "<%= prop() %>",
-            hasParent = context.parentNode,
-            relativeHierarchySelector = /^\s*[+~]/.test(query);
-
-        if (relativeHierarchySelector && !hasParent) {
-            return [];
-        }
-        if (!old) {
-            context.setAttribute("id", nid);
-        } else {
-            nid = nid.replace(/'/g, "\\$&");
-        }
-        if (relativeHierarchySelector && hasParent) {
-            context = context.parentNode;
-        }
-        var selectors = query.match(/([^\s,](?:"(?:\\.|[^"])+"|'(?:\\.|[^'])+'|[^,])*)/g),
-            i = 0, len = selectors.length;
-        
-        for (; i < len; i++) {
-            selectors[i] = "[id='" + nid + "'] " + selectors[i];
-        }
-        query = selectors.join(",");
-
-        try {
-            return method.call(context, query);
-        } finally {
-            if (!old) {
-                oldContext.removeAttribute("id");
-            }
-        }
-    };
+var rsibling = /[\x20\t\r\n\f]*[+~>]/,
+    rquick = DOCUMENT.getElementsByClassName ? /^(?:(\w+)|\.([\w\-]+))$/ : /^(?:(\w+))$/,
+    rescape = /'|\\/g;
 
 implement({
     // Find the first matched element by css selector
@@ -50,32 +14,49 @@ implement({
     queryAll: "All"
 
 }, (methodName, all) => function(selector) {
-    if (!is(selector, "string")) minErr(methodName + "()", ERROR_MSG[4]);
+    if (typeof selector !== "string") minErr();
 
-    var node = this[0],
+    var node = this[ 0 ],
         quickMatch = rquick.exec(selector),
         result, old, nid, context;
-    
+
     if (quickMatch) {
-    
-        if (quickMatch[1]) {
+        if (quickMatch[ 1 ]) {
             // speed-up: "TAG"
-            result = node.getElementsByTagName(selector);
+            result = node.getElementsByTagName( selector );
         } else {
             // speed-up: ".CLASS"
-            result = node.getElementsByClassName(quickMatch[2]);
+            result = node.getElementsByClassName( quickMatch[ 2 ] );
         }
-        if (result && !all) result = result[0];
-    
+
+        if (result && !all) result = result[ 0 ];
     } else {
+        old = true;
+        context = node;
 
         if (node !== node.ownerDocument.documentElement) {
-            result = useRoot(node, selector, node["querySelector" + all]);
-        } else {
-            result = invoke(node, "querySelector" + all, selector);
+            // qSA works strangely on Element-rooted queries
+            // We can work around this by specifying an extra ID on the root
+            // and working up from there (Thanks to Andrew Dupont for the technique)
+            if ( (old = node.getAttribute( "id" )) ) {
+                nid = old.replace( rescape, "\\$&" );
+            } else {
+                nid = "<%= prop('ugma') %>";
+                node.setAttribute("id", nid);
+            }
+
+            nid = "[id='" + nid + "'] ";
+            
+            context = rsibling.test(selector) ? node.parentNode : node;
+            
+            selector = nid + selector.split(",").join("," + nid);
         }
+
+        result = invoke(context, "querySelector" + all, selector);
+
+        if (!old) node.removeAttribute("id");
     }
 
-    return all ? map(result, nodeTree) : nodeTree(result);
-    
+        return all ? map(result, nodeTree) : nodeTree(result);
+        
 }, (methodName, all) => () => all ? [] : new dummyTree());
