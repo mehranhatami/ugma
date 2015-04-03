771756ec138649cfa6df4c5fd46946243c4b5eff
KFlash <kflash123@gmail.com>
2015-04-03 20:42:32 +0800

    simplified rAF

--- src/util/raf.js
+++ src/util/raf.js
@@ -5,35 +5,31 @@
 import { WINDOW } from "../const";
 
 var lastTime = 0,
-    requestAnimationFrame = WINDOW.requestAnimationFrame             ||
-                            WINDOW.mozRequestAnimationFrame          ||
-                            WINDOW.webkitRequestAnimationFrame,
-    cancelAnimationFrame =  WINDOW.cancelAnimationFrame              ||
-                            WINDOW.webkitCancelAnimationFrame        ||
-                            WINDOW.webkitCancelRequestAnimationFrame,
-
-    requestFrame = ( callback ) => {
-        if ( requestAnimationFrame ) {
-            return requestAnimationFrame( callback );
-        } else {
-            // Dynamically set delay on a per-tick basis to match 60fps.
-            var currTime = Date.now(),
-                timeDelay = Math.max( 0, 16 - ( currTime - lastTime ) ); // 1000 / 60 = 16.666
-
-            lastTime = currTime + timeDelay;
-
-            return WINDOW.setTimeout( () => { callback(currTime + timeDelay) }, timeDelay);
-        }
+    requestAnimationFrame =
+          WINDOW.requestAnimationFrame            ||
+          WINDOW.webkitRequestAnimationFrame      ||
+          WINDOW.mozRequestAnimationFrame         ||
+    cancelAnimationFrame = 
+         WINDOW.cancelAnimationFrame              ||
+         WINDOW.webkitCancelAnimationFrame        ||
+         WINDOW.webkitCancelRequestAnimationFrame,
+         WINDOW.mozCancelAnimationFrame           ||
+    requestFrame = requestAnimationFrame ||
+      function( callback ) {
+        // Dynamically set delay on a per-tick basis to match 60fps.
+        var currTime = Date.now(),
+            timeDelay = Math.max( 0, 16 - ( currTime - lastTime ) ); // 1000 / 60 = 16.666
+        lastTime = currTime + timeDelay;
+        return WINDOW.setTimeout( function() {
+            callback( Date.now() );
+        }, timeDelay );
     },
-    cancelFrame = ( frameId ) => {
-        if ( cancelAnimationFrame ) {
-            cancelAnimationFrame( frameId );
-        } else {
-            WINDOW.clearTimeout( frameId );
-        }
+    cancelFrame = cancelAnimationFrame || 
+      function( frameId ) {
+        WINDOW.clearTimeout( frameId );
     };
 
 // Works around a rare bug in Safari 6 where the first request is never invoked.
-requestFrame( () => { return () => {} } );
+requestFrame( () => () => {} );
 
 export { requestFrame, cancelFrame };
