e2c45a29ecc7c46a4d24b101b375933cbb2a55b9
KFlash <kflash123@gmail.com>
2015-04-04 20:49:17 +0800

    noConflict() cleanup

--- src/core/noConflict.js
+++ src/core/noConflict.js
@@ -5,14 +5,21 @@
 import { WINDOW } from "../const";
 import { ugma   } from "../core/core";
 
-// Map over 'ugma' in case of overwrite
+/*
+ * Map over 'ugma' the previous value of the global `ugma` variable, so that it can be restored later on.
+ */
+
 var _ugma = WINDOW.ugma;
 
-// Runs ugma in *noConflict* mode, returning the original `ugma` namespace.
-ugma.noConflict = function() {
-    if ( WINDOW.ugma === ugma ) {
-        WINDOW.ugma = _ugma;
-    }
+/**
+ * In case another library sets the `ugma` variable before ugma does,
+ * this method can be used to return the original `ugma` namespace to that other library.
+ *
+ * @return {Object} Reference to ugma.
+ * @example
+ *     var ugma = ugma.noConflict();
+ */
 
+    if ( WINDOW.ugma === ugma ) WINDOW.ugma = _ugma;
     return ugma;
 };
