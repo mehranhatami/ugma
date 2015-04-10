edfb0064650caebfde20a00f35c6197eef24402a
KFlash <kflash123@gmail.com>
2015-04-10 20:51:17 +0800

    refactor XHR

--- plugins/xhr/xhr.js
+++ plugins/xhr/xhr.js
@@ -1,4 +1,4 @@
-(function(window) {
+(function(ugma) {
 
     "use strict";
 
@@ -8,28 +8,32 @@
         isArray = Array.isArray,
         keys = Object.keys,
         toString = Object.prototype.toString,
-        isObject = function( o ) {
-            return toString.call( o ) === "[object Object]"
+        isObject = function( obj ) {
+            return toString.call( obj ) === "[object Object]"
         },
-        toQueryString = function( params ) {
-            return params.join("&").replace(/%20/g, "+")
-        },
-        mimeTypeShortcuts = {
-            json: APPLICATION_JSON
-        },
-        mimeTypeStrategies = {};
+        toQueryString = function( params ) { return params.join( "&" ).replace( /%20/g, "+" ) },
+        mimeTypeShortcuts = { json: APPLICATION_JSON },
+        mimeTypeStrategies = {
+            "application/json": function(text) {
+                return JSON.parse(text);
+            }
+        };
 
-    mimeTypeStrategies[ APPLICATION_JSON ] = function( text ) {
-        return JSON.parse( text )
-    };
+    if ( !window.ugma ) console.warn( "ugma javascript Framework need to be included" );
+
+    if ( !Promise ) ugma.minErr( "XHR()", "In order to use XHR you have to include a Promise polyfill" );
 
     function isSuccess( status ) {
         return 200 <= status && status < 300 || 304;
     }
 
+    // main XHR function
     function XHR( method, url ) {
+
         var options = arguments[ 2 ];
+
         if ( options === void 0 ) options = {};
+
         method = method.toUpperCase();
 
         var charset = "charset" in options ? options.charset : XHR.defaults.charset,
@@ -39,7 +43,7 @@
             headers = {};
 
         // read default headers first
-        keys( XHR.defaults.headers ).forEach(function( key ) {
+        keys( XHR.defaults.headers).forEach( function( key ) {
             headers[ key ] = XHR.defaults.headers[ key ];
         });
 
@@ -48,13 +52,13 @@
             headers[ key ] = options.headers[ key ];
         });
 
-        if (isObject(data)) {
-            keys( data ).forEach( function( key ) {
+        if ( isObject( data ) ) {
+            keys( data ).forEach(function(key) {
                 var name = encodeURIComponent( key ),
                     value = data[ key ];
 
                 if ( isArray( value ) ) {
-                    value.forEach( function( value ) {
+                    value.forEach(function( value ) {
                         extraArgs.push( name + "=" + encodeURIComponent( value ) );
                     });
                 } else {
@@ -71,7 +75,7 @@
         }
 
         if ( typeof data === "string" ) {
-            if (method === "GET" ) {
+            if ( method === "GET" ) {
                 extraArgs.push( data );
 
                 data = null;
@@ -80,14 +84,14 @@
             }
         }
 
-        if (isObject(options.json)) {
-            data = JSON.stringify(options.json);
+        if ( isObject( options.json ) ) {
+            data = JSON.stringify( options.json );
 
-            headers[contentType] = APPLICATION_JSON;
+            headers[ contentType ] = APPLICATION_JSON;
         }
 
         if (contentType in headers) {
-            headers[contentType] += "; charset=" + charset;
+            headers[ contentType ] += "; charset=" + charset;
         }
 
         // For older servers, emulate HTTP by mimicking the HTTP method with `_method`
@@ -101,29 +105,29 @@
         if ( extraArgs.length ) {
             url += ( ~url.indexOf( "?" ) ? "&" : "?" ) + toQueryString( extraArgs );
         }
-       
+
         // Make the request, allowing the user to override any Ajax options.
-        var xhr = options.xhr = new XMLHttpRequest(),
-            promise = new Promise(function( resolve, reject ) {
+        var xhr = options.xhr || new XMLHttpRequest(),
+            promise = new Promise( function( resolve, reject ) {
                 var handleErrorResponse = function( message ) {
                     return function() {
                         reject( new Error( message ) )
                     }
                 };
 
-                xhr.open( method, 
-                         url, 
-                         true
-                        // username ?
-                        // password ?
-                        );
-                
+                xhr.open(method,
+                    url,
+                    true
+                    // username ?
+                    // password ?
+                );
+
                 xhr.timeout = options.timeout || XHR.defaults.timeout;
 
                 // Set headers
                 for ( var key in headers ) {
 
-                    var headerValue = headers[ key ] ;
+                    var headerValue = headers[ key ];
 
                     if ( headerValue != null ) {
                         xhr.setRequestHeader( key, String( headerValue ) );
@@ -140,11 +144,11 @@
                     }
                 }
 
-                xhr.onabort = handleErrorResponse("abort");
-                xhr.onerror = handleErrorResponse("error");
-                xhr.ontimeout = handleErrorResponse("timeout");
+                xhr.onabort = handleErrorResponse( "abort" );
+                xhr.onerror = handleErrorResponse( "error" );
+                xhr.ontimeout = handleErrorResponse( "timeout" );
                 xhr.onreadystatechange = function() {
-                  
+
                     if ( xhr.readyState === 4 ) {
                         // by default parse response depending on Content-Type header
                         mimeType = mimeType || xhr.getResponseHeader( contentType ) || "";
@@ -157,18 +161,18 @@
                             // http://stackoverflow.com/questions/10046972/msie-returns-status-code-of-1223-for-ajax-request
                             status = xhr.status === 1223 ? 204 : xhr.status,
                             // skip possible charset suffix
-                            parseResponse = mimeTypeStrategies[ mimeType.split(";")[ 0 ] ];
+                            parseResponse = mimeTypeStrategies[ mimeType.split( ";" )[ 0 ] ];
 
                         if ( parseResponse ) {
                             try {
                                 // when strategy found - parse response according to it
                                 response = parseResponse( response );
                             } catch ( err ) {
-                                return reject( err );
+                                return reject(err);
                             }
                         }
 
-                        if ( isSuccess( status) ) {
+                        if ( isSuccess( status ) ) {
                             resolve( response );
                         } else {
                             reject( response );
@@ -176,7 +180,7 @@
                     }
                 };
 
-                xhr.send(data || null );
+                xhr.send( data || null );
             });
 
         promise[ 0 ] = xhr;
@@ -185,7 +189,7 @@
     }
 
     // define shortcuts
-    [ "get", "post", "put", "delete", "patch" ].forEach(function( method ) {
+    [ "get", "post", "put", "delete", "patch" ].forEach( function( method ) {
         XHR[ method ] = function( url, options ) {
             return XHR( method, url, options )
         };
@@ -201,10 +205,82 @@
         }
     };
 
-    if (Promise) {
-        // expose namespace globally
-        window.XHR = XHR;
-    } else {
-        throw new Error("In order to use XHR you have to include a Promise polyfill");
-    }
-})(window);
+    /* Expose */
+
+    ugma.extend({
+
+        XHR: XHR,
+
+        serialize: function(node) {
+
+            var result = {};
+
+            if ("form" in node) {
+                node = [node];
+            } else if ("elements" in node) {
+                node = node.elements;
+            } else {
+                node = [];
+            }
+
+            var index = 0,
+                length = node.length;
+
+            for (var el; index < length;) {
+                el = ( node[ index++ ] );
+
+                var name = el.name;
+
+                if ( el.disabled || !name ) continue;
+
+                switch ( el.type ) {
+                    case "select-multiple":
+                        result[name] = [];
+                        /* falls through */
+                    case "select-one":
+
+                        var option, opts = ( el.options );
+
+                        for (index = 0, length = opts.length; index < length;) {
+                            option = ( opts[ index++ ] );
+
+                            if ( option.selected ) {
+                                if ( name in result ) {
+                                    result[ name ].push( option.value );
+                                } else {
+                                    result[ name ] = option.value;
+                                }
+                            }
+                        };
+                        break;
+
+                    case undefined:
+                    case "fieldset": // fieldset
+                    case "file": // file input
+                    case "submit": // submit button
+                    case "reset": // reset button
+                    case "button": // custom button
+                        break;
+
+                    case "checkbox": // checkbox
+                        if ( el.checked && result[ name ] ) {
+                            if ( typeof result[ name ] === "string" ) {
+                                result[ name ] = [ result[ name ] ];
+                            }
+
+                            result[ name ].push( el.value );
+
+                            break;
+                        }
+                    case "radio": // radio button
+                        if ( !el.checked ) break;
+                    default:
+                        result[ name ] = el.value;
+                }
+            };
+
+            return result;
+        }
+    });
+
+})( window.ugma );
--- src/modules/global.js
+++ src/modules/global.js
@@ -4,11 +4,13 @@
 import { implement                           } from "../core/core";
 import { camelize, computeStyle, proxy, is   } from "../helpers";
 import { deserializeValue                    } from "../util/readData";
+import { minErr                              } from "../minErr";
   
   // All API functions we want expose to the 'ugma namespace', or
   // the .shadow() DOM trees, we list here
 
 implement({
+      minErr: minErr,
       camelize: camelize,
       computeStyle: computeStyle,
       proxy: proxy,
