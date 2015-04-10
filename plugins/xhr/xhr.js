5d98ea1810cb07e5567615627ccebc82837775c8
KFlash <kflash123@gmail.com>
2015-04-10 17:20:03 +0800

    XHR cleanup

--- plugins/xhr/xhr.js
+++ plugins/xhr/xhr.js
@@ -8,10 +8,10 @@
         isArray = Array.isArray,
         keys = Object.keys,
         toString = Object.prototype.toString,
-        isObject = function(o) {
-            return toString.call(o) === "[object Object]"
+        isObject = function( o ) {
+            return toString.call( o ) === "[object Object]"
         },
-        toQueryString = function(params) {
+        toQueryString = function( params ) {
             return params.join("&").replace(/%20/g, "+")
         },
         mimeTypeShortcuts = {
@@ -19,17 +19,17 @@
         },
         mimeTypeStrategies = {};
 
-    mimeTypeStrategies[APPLICATION_JSON] = function(text) {
-        return JSON.parse(text)
+    mimeTypeStrategies[ APPLICATION_JSON ] = function( text ) {
+        return JSON.parse( text )
     };
 
-    function isSuccess(status) {
+    function isSuccess( status ) {
         return 200 <= status && status < 300 || 304;
     }
 
-    function XHR(method, url) {
-        var options = arguments[2];
-        if (options === void 0) options = {};
+    function XHR( method, url ) {
+        var options = arguments[ 2 ];
+        if ( options === void 0 ) options = {};
         method = method.toUpperCase();
 
         var charset = "charset" in options ? options.charset : XHR.defaults.charset,
@@ -39,44 +39,44 @@
             headers = {};
 
         // read default headers first
-        keys(XHR.defaults.headers).forEach(function(key) {
-            headers[key] = XHR.defaults.headers[key];
+        keys( XHR.defaults.headers ).forEach(function( key ) {
+            headers[ key ] = XHR.defaults.headers[ key ];
         });
 
         // apply request specific headers
-        keys(options.headers || {}).forEach(function(key) {
-            headers[key] = options.headers[key];
+        keys( options.headers || {} ).forEach(function( key ) {
+            headers[ key ] = options.headers[ key ];
         });
 
         if (isObject(data)) {
-            keys(data).forEach(function(key) {
-                var name = encodeURIComponent(key),
-                    value = data[key];
+            keys( data ).forEach( function( key ) {
+                var name = encodeURIComponent( key ),
+                    value = data[ key ];
 
-                if (isArray(value)) {
-                    value.forEach(function(value) {
-                        extraArgs.push(name + "=" + encodeURIComponent(value));
+                if ( isArray( value ) ) {
+                    value.forEach( function( value ) {
+                        extraArgs.push( name + "=" + encodeURIComponent( value ) );
                     });
                 } else {
-                    extraArgs.push(name + "=" + encodeURIComponent(value));
+                    extraArgs.push( name + "=" + encodeURIComponent( value ) );
                 }
             });
 
-            if (method === "GET") {
+            if ( method === "GET" ) {
                 data = null;
             } else {
-                data = toQueryString(extraArgs);
+                data = toQueryString( extraArgs );
                 extraArgs = [];
             }
         }
 
-        if (typeof data === "string") {
-            if (method === "GET") {
-                extraArgs.push(data);
+        if ( typeof data === "string" ) {
+            if (method === "GET" ) {
+                extraArgs.push( data );
 
                 data = null;
             } else {
-                headers[contentType] = "application/x-www-form-urlencoded";
+                headers[ contentType ] = "application/x-www-form-urlencoded";
             }
         }
 
@@ -92,45 +92,51 @@
 
         // For older servers, emulate HTTP by mimicking the HTTP method with `_method`
         // And an `X-HTTP-Method-Override` header.
-        if (options.emulateHTTP && (method === 'PUT' || method === 'DELETE' || method === 'PATCH' || method === 'POST' || method === 'GET')) {
-            extraArgs.push(options.emulateHTTP + "=" + method);
-            headers["X-Http-Method-Override"] = method;
+        if ( options.emulateHTTP && (method === 'PUT' || method === 'DELETE' || method === 'PATCH' || method === 'POST' || method === 'GET' ) ) {
+            extraArgs.push( options.emulateHTTP + "=" + method );
+            headers[ "X-Http-Method-Override" ] = method;
             method = "POST";
         }
 
-        if (extraArgs.length) {
-            url += (~url.indexOf("?") ? "&" : "?") + toQueryString(extraArgs);
+        if ( extraArgs.length ) {
+            url += ( ~url.indexOf( "?" ) ? "&" : "?" ) + toQueryString( extraArgs );
         }
        
         // Make the request, allowing the user to override any Ajax options.
         var xhr = options.xhr = new XMLHttpRequest(),
-            promise = new Promise(function(resolve, reject) {
-                var handleErrorResponse = function(message) {
+            promise = new Promise(function( resolve, reject ) {
+                var handleErrorResponse = function( message ) {
                     return function() {
-                        reject(new Error(message))
+                        reject( new Error( message ) )
                     }
                 };
 
-                xhr.open(method, url, true);
+                xhr.open( method, 
+                         url, 
+                         true
+                        // username ?
+                        // password ?
+                        );
+                
                 xhr.timeout = options.timeout || XHR.defaults.timeout;
 
-                // set request headers
-                keys(headers).forEach(function(key) {
+                // Set headers
+                for ( var key in headers ) {
 
-                    var headerValue = headers[key];
+                    var headerValue = headers[ key ] ;
 
-                    if (headerValue != null) {
-                        xhr.setRequestHeader(key, String(headerValue));
+                    if ( headerValue != null ) {
+                        xhr.setRequestHeader( key, String( headerValue ) );
                     }
-                });
+                }
 
-                // set mime type
-                if (mimeType) {
-                    if (mimeType in mimeTypeShortcuts) {
+                // Override mime type if needed
+                if ( mimeType ) {
+                    if ( mimeType in mimeTypeShortcuts ) {
                         xhr.responseType = mimeType;
-                        mimeType = mimeTypeShortcuts[mimeType];
-                    } else if (xhr.overrideMimeType) {
-                        xhr.overrideMimeType(mimeType);
+                        mimeType = mimeTypeShortcuts[ mimeType ];
+                    } else if ( xhr.overrideMimeType ) {
+                        xhr.overrideMimeType( mimeType );
                     }
                 }
 
@@ -138,47 +144,50 @@
                 xhr.onerror = handleErrorResponse("error");
                 xhr.ontimeout = handleErrorResponse("timeout");
                 xhr.onreadystatechange = function() {
-                    if (xhr.readyState === 4) {
+                  
+                    if ( xhr.readyState === 4 ) {
                         // by default parse response depending on Content-Type header
-                        mimeType = mimeType || xhr.getResponseHeader(contentType) || "";
+                        mimeType = mimeType || xhr.getResponseHeader( contentType ) || "";
 
                         // responseText is the old-school way of retrieving response (supported by IE8 & 9)
                         // response/responseType properties were introduced in XHR Level2 spec (supported by IE10)
-                        var response = response = ('response' in xhr) ? xhr.response : xhr.responseText,
+                        var response = response = ( 'response' in xhr ) ? xhr.response : xhr.responseText,
+                            // Support: IE9
+                            // sometimes IE returns 1223 when it should be 204
                             // http://stackoverflow.com/questions/10046972/msie-returns-status-code-of-1223-for-ajax-request
                             status = xhr.status === 1223 ? 204 : xhr.status,
                             // skip possible charset suffix
-                            parseResponse = mimeTypeStrategies[mimeType.split(";")[0]];
+                            parseResponse = mimeTypeStrategies[ mimeType.split(";")[ 0 ] ];
 
-                        if (parseResponse) {
+                        if ( parseResponse ) {
                             try {
                                 // when strategy found - parse response according to it
-                                response = parseResponse(response);
-                            } catch (err) {
-                                return reject(err);
+                                response = parseResponse( response );
+                            } catch ( err ) {
+                                return reject( err );
                             }
                         }
 
-                        if (isSuccess(status)) {
-                            resolve(response);
+                        if ( isSuccess( status) ) {
+                            resolve( response );
                         } else {
-                            reject(response);
+                            reject( response );
                         }
                     }
                 };
 
-                xhr.send(data || null);
+                xhr.send(data || null );
             });
 
-        promise[0] = xhr;
+        promise[ 0 ] = xhr;
 
         return promise;
     }
 
     // define shortcuts
-    ["get", "post", "put", "delete", "patch"].forEach(function(method) {
-        XHR[method] = function(url, options) {
-            return XHR(method, url, options)
+    [ "get", "post", "put", "delete", "patch" ].forEach(function( method ) {
+        XHR[ method ] = function( url, options ) {
+            return XHR( method, url, options )
         };
     });
 
@@ -187,6 +196,7 @@
         timeout: 15000,
         charset: "UTF-8",
         headers: {
+            // X-Requested-With header
             "X-Requested-With": "XMLHttpRequest"
         }
     };
