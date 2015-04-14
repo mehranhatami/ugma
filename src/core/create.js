e59b2f12d09d1dfe5083307710a6f8d7ff3a397e
Mehran Hatami <mehranhatamie@gmail.com>
2015-04-14 12:42:33 +0430

    fix create.js

--- src/core/create.js
+++ src/core/create.js
@@ -5,9 +5,9 @@ function isObject(value) {
   var type = typeof value;
   return type === "function" || (value && type === "object") || false;
 }
-var baseCreate = (() => {
+var baseCreate = (function(){
   function UgmaObject() {}
-  return (prototype) => {
+  return function(prototype) {
     var result;
     if (isObject(prototype)) {
       UgmaObject.prototype = prototype;
